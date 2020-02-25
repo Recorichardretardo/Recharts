92977917bf856440f0e8e07c274cfa6c7c50e907
K Kasi <you@example.com>
2020-02-26 00:54:16 +0530

    custom tooltip

--- src/pages/LineChart/index.js
+++ src/pages/LineChart/index.js
@@ -1,238 +1,205 @@
-/* eslint-disable react/no-multi-comp */
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer
-} from 'recharts';
-import './index.scss';
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ReferenceLine
+} from "recharts";
+import * as moment from "moment";
 
-
-const data = [
+const series = [
   {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
+    name: "Actual Order",
+    data: [
+      { orderDate: "1", value: 8 },
+      { orderDate: "2", value: 5 },
+      { orderDate: "3", value: 5.5 },
+      { orderDate: "4", value: 4.25 }
+    ],
+    stroke: "#ffc220"
   },
   {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    name: "HVI Prediction",
+    data: [
+      { orderDate: "1", value: 6 },
+      { orderDate: "2", value: 5.5 },
+      { orderDate: "3", value: 4.25 },
+      { orderDate: "4", value: 5 }
+    ],
+    stroke: "#a0b5e8"
   },
   {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Page Asssssssssssssss', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
+    name: "HVI Order",
+    data: [
+      { orderDate: "8", value: 6 },
+      { orderDate: "9", value: 4.5, HVIRecommendedOrder: true }
+    ],
+    stroke: "#0065ff",
+    strokeDasharray: "7 7"
+  }
 ];
 
-class CustomizedLabel extends PureComponent {
-  render() {
-    const {
-      x, y, stroke, value,
-    } = this.props;
+const CustomizedDot = props => {
+  const { cx, cy, stroke, payload } = props;
 
-    return <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">{value}
-   
-    </text>;
-  }
-}
-
-class CustomizedAxisTick extends PureComponent {
-  render() {
-    const {
-      x, y, stroke, payload,
-    } = this.props;
-
+  if (!payload.HVIRecommendedOrder) {
     return (
-      <g transform={`translate(${x},${y})`}>
-        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value.substring(1, 4)}
-        <title><b>{payload.value}</b> </title>
-        </text>
-      </g>
+      <svg
+        x={cx - 10}
+        y={cy - 10}
+        width={40}
+        height={40}
+        fill={stroke}
+        viewBox="0 0 200 200"
+      >
+        <circle cx="50" cy="50" r="40" strokeWidth="3" fill={stroke} />
+      </svg>
     );
   }
-}
+
+  return (
+    <svg x={cx - 20} y={cy - 15} width={40} height={40} viewBox="0 0 200 200">
+      <rect
+        width="15"
+        height="15"
+        fill="white"
+        stroke={stroke}
+        strokeWidth="2"
+        transform="matrix(3 3 -3 3 100 25)"
+      />
+    </svg>
+  );
+};
+
+const CustomLine = props => {
+  const { x, y } = props.viewBox;
+  return (
+    <text
+      fontSize="10px"
+      fontWeight="bold"
+      fill="green"
+      x={x}
+      y={y - 10}
+      className="recharts-text recharts-label"
+      textAnchor="middle"
+    >
+      <tspan x={x} dy="0.355em">
+        Activated 4/10
+      </tspan>
+    </text>
+  );
+};
 
 export default class Example extends PureComponent {
-  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/5br7g9d6/';
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: undefined
+    };
+    this.tooltip = null;
+    this.tooltipQty = null;
+    this.tooltipDate = null;
+  }
+
+  customMouseOver = (e, name) => {
+    if(name.name === "HVI Prediction" ) {
+      this.setState({
+        name:1
+      })
+    }else {
+      this.setState({
+        name:0
+      })
+    }
+   
+   
+  };
+
+  over = e => {
+   
+  };
+
+  xAxisTickFormatter(month) {
+    return month;
+  }
+  renderTooltip = ({ active, payload, label }) => {
+    if (active) {
+      let nae;
+      if(this.state.name !==undefined){
+        nae=this.state.name
+          
+      }else {
+        nae=0
+      }
+      return (
+       
+        <div className="custom-tooltip">
+          <p className="label">{`${label} : ${payload[nae].value}`}</p>
+        </div>
+      );
+    }
+  
+    return null;
+
+  }
 
   render() {
     return (
-      <>
-      <div  id="container" className="chartAreaWrapper">
-      <ResponsiveContainer height={500} width="100%">
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 20, right: 30, left: 20, bottom: 10,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" interval={0} height={60} dx={20}
-         allowDataOverflowBoolean={true} padding={{ left: 20, right: 20 }} tick={<CustomizedAxisTick />} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="linear" dataKey="pv" stroke="#8884d8" label={<CustomizedLabel />} />
-        <Line type="linear" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
-      </ResponsiveContainer>
+      <div>
+        <LineChart width={900} height={300} margin={{ top: 20, right: 20 }}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="orderDate"
+            tick={{ fontSize: 14, fontWeight: "bold" }}
+            domain={[1, 12]}
+            tickCount={12}
+            tickFormatter={this.xAxisTickFormatter}
+            type="number"
+            tickLine={false}
+            allowDuplicatedCategory={false}
+            padding={{ left: 30, right: 30 }}
+          />
+          <YAxis
+            dataKey="value"
+            tick={{ fontSize: 13, fill: "#6d6e71" }}
+            domain={[0, "dataMax + 12"]}
+            tickCount={20}
+            allowDecimals={false}
+            tickLine={false}
+          />
+          <Tooltip  content={this.renderTooltip} />
+          <ReferenceLine
+            x="7"
+            strokeDasharray="5,5"
+            stroke="green"
+            label={<CustomLine />}
+          />
+          {series.map(s => (
+            <Line
+              activeDot={{
+                onMouseOver: e => this.customMouseOver(e, s),
+                onMouseLeave: this.over
+              }}
+              isAnimationActive={false}
+              dataKey="value"
+              dot={<CustomizedDot />}
+              stroke={s.stroke}
+              strokeDasharray={s.strokeDasharray}
+              data={s.data}
+              name={s.name}
+              key={s.name}
+            />
+          ))}
+        </LineChart>
       </div>
-      </>
     );
   }
 }
+
+
+const CustomTooltip = ({ active, payload, label }) => {
+ 
+};
