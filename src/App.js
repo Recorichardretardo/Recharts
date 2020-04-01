abd3e73737dae505172a07b9aab3b9a818ef09b4
K Kasi <you@example.com>
2020-04-01 23:45:47 +0530

    recharts area chart examples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 //import ExJsplumb from './Zoom2'; // Change Example name
-import Grid from './pages/Table';
+import Grid from './pages/AreaChart';
 
 class App extends React.Component{
   render() {
