8622a163c233fb27d96decf133f7734a5a7e0362
K Kasi <you@example.com>
2020-04-04 20:12:08 +0530

    Time zone utc string

--- src/pages/TimeZones/index.js
+++ src/pages/TimeZones/index.js
@@ -11,27 +11,60 @@ import moment from 'moment';
   }
   componentDidMount() {
     
-    let date = new Date();
-    console.log("date : "+date)
-    // 2020-04-04T07:53:00+00:00
-    let dateFormat = moment(date).format("YYYY-MM-DDTHH:mm:ss+00:00")
-    console.log("dateFormat : "+dateFormat)
+    // let date = new Date();
+    // console.log("date : "+date)
+    // // 2020-04-04T07:53:00+00:00
+    // let dateFormat = moment(date).format("YYYY-MM-DDTHH:mm:ss+00:00")
+    // console.log("dateFormat : "+dateFormat)
+    // let pstOffSet = -8*60
+    //  date.setMinutes(date.getMinutes() + pstOffSet)
+    //  console.log("PST : "+date)
+    // let PSTFormat = moment(date).format("YYYY-MM-DDTHH:mm:ss+00:00")
+    // console.log("PSTFormat TO  : "+PSTFormat)
+    // let sevenDay = new Date(date.getTime() - (1000*60*60*7*24));
+    // let PSTSevenDaysFormat = moment(sevenDay).format("YYYY-MM-DDTHH:mm:ss+00:00")
+    // console.log("PSTFormat From : "+PSTSevenDaysFormat)
 
+    console.log("============CURRENT DATE IST ==============\n")
+      let currentDate = new Date();
+      console.log("currentDate : ",currentDate);
+      let currentDateFormat = moment(currentDate).format("YYYY-MM-DDTHH:mm:ss+00:00")
+      console.log("currentDateFormat : ",currentDateFormat);
+      console.log("============CURRENT DATE UTC ==============\n") 
+      let utcDate = currentDate.toUTCString();
+      console.log("utcDate :  ",utcDate); 
+      let utcDateFormat = moment(utcDate).utc().format("YYYY-MM-DDTHH:mm:ss+00:00")
+      console.log("utcDateFormat : ",utcDateFormat);
+      // console.log("============CURRENT DATE GMT ==============/n") 
+      // let gmtDate = currentDate.toGMTString();
+      //  console.log("gmtDate : ",gmtDate)
+      // let gmtDateFormat = moment(gmtDate).format("YYYY-MM-DDTHH:mm:ss+00:00")
+      // console.log("gmtDateFormat : ",gmtDateFormat);
+    console.log("===========================================================================================\n");
     let pstOffSet = -8*60
 
-     date.setMinutes(date.getMinutes() + pstOffSet)
+    console.log("============CURRENT DATE IST PT TIME ZONE ==============\n")
 
-    console.log("PST : "+date)
-    let PSTFormat = moment(date).format("YYYY-MM-DDTHH:mm:ss+00:00")
+    let currentPTDATE = new Date();
+     currentPTDATE.setMinutes(currentPTDATE.getMinutes() + pstOffSet);
 
-    console.log("PSTFormat TO  : "+PSTFormat)
+     console.log("currentPTDATE : ",currentPTDATE);
 
-    let sevenDay = new Date(date.getTime() - (1000*60*60*7*24));
+     console.log("============CURRENT DATE UTC PT TIME ZONE ==============\n")
+     let ptOffSet = -12*60
+    let utcPTDATE = new Date();
+    utcPTDATE.setUTCMinutes(utcPTDATE.getUTCMinutes() + ptOffSet);
 
-    let PSTSevenDaysFormat = moment(sevenDay).format("YYYY-MM-DDTHH:mm:ss+00:00")
+     console.log("utcPTDATE : ",utcPTDATE);
 
-    console.log("PSTFormat From : "+PSTSevenDaysFormat)
    
+
+    
+
+
+    
+ 
+    
   }
 
   
